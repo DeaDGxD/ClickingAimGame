5e4d2c1aff18895ffc80151869d7873b9e652c4d
DeaDGxD <59544724+DeaDGxD@users.noreply.github.com>
2020-03-19 01:03:58 -0500

    First

---
+++ static/myScript.js
@@ -0,0 +1,32 @@
+let doc = document.getElementById("place");
+let x = doc.offsetHeight - 100;
+let y = doc.offsetWidth - 100;
+let hits = 0;
+let miss = 0;
+document.getElementById("Shooter").addEventListener("click", hit);
+function move(x,y) {
+    let height = Math.floor(Math.random() * x);
+    let width = Math.floor(Math.random() * y);
+    let fin = [height, width];
+    return fin;
+}
+function newMove() {
+    let el = document.getElementById("Shoot");
+    let pos = move(x,y);
+    el.style.left = pos[1] + "px";
+    el.style.top = pos[0] + "px";
+}
+function hit() {
+    hits = hits+1;
+    let h = document.getElementById("hits");
+    h.innerHTML = "Times hit:"+hits.toString();
+    let accuracy = (hits / (hits + miss) * 100).toString() + "%";
+    let m = document.getElementById("missing");
+    m.innerHTML = "Times missed: "+miss.toString() + " " + "Accuracy: " + accuracy;
+}
+function missy() {
+    let accuracy = (hits / (hits + miss) * 100).toString() + "%";
+    miss = miss + 1;
+    let m = document.getElementById("missing");
+    m.innerHTML = "Times missed: "+miss.toString() + " " + "Accuracy: " + accuracy;
+}
